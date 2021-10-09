be38021c493abd116f2a068f8917fe3875c4146f
huangzhhui <huangzhwork@gmail.com>
2021-10-09 19:36:41 +0800

    PHP 8 Only

--- src/Requests/BaseRequest.php
+++ src/Requests/BaseRequest.php
@@ -11,13 +11,12 @@ declare(strict_types=1);
  */
 namespace Hyperf\XxlJob\Requests;
 
+use JetBrains\PhpStorm\Pure;
+
 class BaseRequest
 {
-    /**
-     * @param array $data
-     * @return static
-     */
-    public static function create(array $data = [])
+    #[Pure]
+    public static function create(array $data = []): static
     {
         $obj = new static();
         foreach ($data as $k => $v) {
